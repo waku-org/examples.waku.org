6e4fbccbcc2da8df7a919fcb8d99c54f7499b440
fryorcraken.eth <git@fryorcraken.xyz>
2022-09-02 15:23:59 +1000

    feat(angular): replace protons with protobufjs
    As latest protons version needs to use a prot file which is not very practical for a quick example.

--- relay-angular-chat/src/app/messages/messages.component.ts
+++ relay-angular-chat/src/app/messages/messages.component.ts
@@ -1,61 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { WakuService } from '../waku.service';
-import { Waku, WakuMessage } from 'js-waku';
-import protons from 'protons';
+import { Component, OnInit } from "@angular/core";
+import { WakuService } from "../waku.service";
+import { Waku, WakuMessage } from "js-waku";
+import protobuf from "protobufjs";
 
-const proto = protons(`
-message SimpleChatMessage {
-  uint64 timestamp = 1;
-  string text = 2;
-}
-`);
+const ProtoChatMessage = new protobuf.Type("ChatMessage")
+  .add(new protobuf.Field("timestamp", 1, "uint32"))
+  .add(new protobuf.Field("text", 2, "string"));
 
 interface MessageInterface {
-  timestamp: Date,
-  text: string
+  timestamp: Date;
+  text: string;
 }
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  selector: "app-messages",
+  templateUrl: "./messages.component.html",
+  styleUrls: ["./messages.component.css"],
 })
 export class MessagesComponent implements OnInit {
-
   contentTopic: string = `/relay-angular-chat/1/chat/proto`;
   messages: MessageInterface[] = [];
   messageCount: number = 0;
   waku!: Waku;
   wakuStatus!: string;
 
-  constructor(private wakuService: WakuService) { }
+  constructor(private wakuService: WakuService) {}
 
   ngOnInit(): void {
-    this.wakuService.wakuStatus.subscribe(wakuStatus => {
+    this.wakuService.wakuStatus.subscribe((wakuStatus) => {
       this.wakuStatus = wakuStatus;
     });
-    
-    this.wakuService.waku.subscribe(waku => {
+
+    this.wakuService.waku.subscribe((waku) => {
       this.waku = waku;
-      this.waku.relay.addObserver(this.processIncomingMessages, [this.contentTopic]);
+      this.waku.relay.addObserver(this.processIncomingMessages, [
+        this.contentTopic,
+      ]);
     });
 
     window.onbeforeunload = () => this.ngOnDestroy();
   }
 
   ngOnDestroy(): void {
-    this.waku.relay.deleteObserver(this.processIncomingMessages, [this.contentTopic]);
+    this.waku.relay.deleteObserver(this.processIncomingMessages, [
+      this.contentTopic,
+    ]);
   }
 
   sendMessage(): void {
     const time = new Date().getTime();
 
-    const payload = proto.SimpleChatMessage.encode({
+    const protoMsg = ProtoChatMessage.create({
       timestamp: time,
       text: `Here is a message #${this.messageCount}`,
     });
 
-    WakuMessage.fromBytes(payload, this.contentTopic).then(wakuMessage => {
+    const payload = ProtoChatMessage.encode(protoMsg).finish();
+
+    WakuMessage.fromBytes(payload, this.contentTopic).then((wakuMessage) => {
       this.waku.relay.send(wakuMessage).then(() => {
         console.log(`Message #${this.messageCount} sent`);
         this.messageCount += 1;
@@ -66,13 +68,13 @@ export class MessagesComponent implements OnInit {
   processIncomingMessages = (wakuMessage: WakuMessage) => {
     if (!wakuMessage.payload) return;
 
-    const { timestamp, text } = proto.SimpleChatMessage.decode(
+    const { text, timestamp } = ProtoChatMessage.decode(
       wakuMessage.payload
-    );
+    ) as unknown as { text: string; timestamp: bigint };
     const time = new Date();
-    time.setTime(timestamp);
+    time.setTime(Number(timestamp));
     const message = { text, timestamp: time };
 
     this.messages.push(message);
-  }
+  };
 }
