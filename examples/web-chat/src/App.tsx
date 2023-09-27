c6e560af0b40de28545b2f0189d07353c14e65c5
Sasha <118575614+weboko@users.noreply.github.com>
2023-09-27 12:41:35 +0200

    chore: revert time change (#276)

--- examples/web-chat/src/App.tsx
+++ examples/web-chat/src/App.tsx
@@ -9,7 +9,7 @@ import { useMessages, usePersistentNick } from "./hooks";
 
 const startTime = new Date();
 // Only retrieve a week of history
-startTime.setTime(Date.now() - 1000 * 60 * 30);
+startTime.setTime(Date.now() - 1000 * 60 * 60 * 24 * 7);
 const endTime = new Date();
 
 export default function App() {
