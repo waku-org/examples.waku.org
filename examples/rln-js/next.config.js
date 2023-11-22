659078e71a11dfd1bbb3a33a650675cd5ba71f10
Sasha <118575614+weboko@users.noreply.github.com>
2023-11-23 01:56:35 +0300

    bug: fix base path (#288)
    * bug: fix base path
    
    * add open script
    
    * up rln

--- examples/rln-js/next.config.js
+++ examples/rln-js/next.config.js
@@ -1,6 +1,9 @@
+const packageJSON = require("./package.json");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/" + packageJSON.name,
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
