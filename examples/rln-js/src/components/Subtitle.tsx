7d0fd843c9c81df66fb43a9ae4cae3741079089f
Sasha <oleksandr@status.im>
2024-01-22 23:00:06 +0100

    remove old rln-js

--- examples/rln-js/src/components/Subtitle.tsx
+++
@@ -1,8 +0,0 @@
-type SubtitleProps = {
-  children: any;
-  className?: string;
-};
-
-export const Subtitle: React.FunctionComponent<SubtitleProps> = (props) => (
-  <h2 className={`text-2xl ${props.className || ""}`}>{props.children}</h2>
-);
--- examples/rln-js/src/components/Title.tsx
+++
@@ -1,8 +0,0 @@
-type TitleProps = {
-  children: any;
-  className?: string;
-};
-
-export const Title: React.FunctionComponent<TitleProps> = (props) => (
-  <h1 className={`text-4xl ${props.className || ""}`}>{props.children}</h1>
-);
