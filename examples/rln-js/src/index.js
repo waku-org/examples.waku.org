cbb436ad35565f09f1392d221972c2ceaa5d21e4
Sasha <oleksandr@status.im>
2024-01-22 23:03:33 +0100

    remove comment

--- examples/rln-js/src/index.js
+++ examples/rln-js/src/index.js
@@ -2,28 +2,6 @@ import { initUI } from "./ui";
 import { initRLN } from "./rln";
 import { initWaku } from "./waku";
 
-/**
-Keystore: easy way to import or hardcode credentials
-
-
-Metamask:
-- abstraction to get provider with triggering dialog from Metamask
-- default: true - embed such abstraction into RLN initialization by default
-- default: false - read provider from options.provider
-
-
-RLN Instance:
-- create and RLNContract are the same;
-- RLNEncoder and RLNDecoder should be just createDecoder and createEncoder and use the one from Waku inside;
-
-
-Verification:
-- embed verification into RLNDecoder
-    reduce: const res = message.verify(rlnContract.roots());
-- fix the problem with double proof generation
-    in preparePushMessage
- */
-
 async function run() {
   const { onLoaded, onStatusChange, registerEvents } = initUI();
   const { encoder, decoder, rlnContract } = await initRLN(onStatusChange);
