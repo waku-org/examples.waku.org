698afe4dab70fed3a8f2d61b6c8c41b80515de51
Sasha <118575614+weboko@users.noreply.github.com>
2024-01-24 20:51:13 +0100

    feat: simplify rln-js (#297)
    * remove old rln-js
    
    * feat: simplify rln-js
    
    * remove comment
    
    * add example to CI

--- examples/rln-js/src/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-export { useStore } from "./useStore";
-export { useRLN } from "./useRLN";
-export { useWallet } from "./useWallet";
-export { useContract } from "./useContract";
-export { useWaku } from "./useWaku";
